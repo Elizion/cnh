d20302019eddbed2d765946ef147f56fedb2ff31
Eli Alvarez <elialvarezromero11@gmail.com>
2019-11-24 11:21:36 -0600

    FEATURE: Add new module recipies

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  private recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl:
+        'https://apod.nasa.gov/apod/image/1911/M101_nasaMultiW.jpg',
+      ingredients: ['French Fries', 'Pork Meat', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl:
+        'https://apod.nasa.gov/apod/image/1911/M101_nasaMultiW.jpg',
+      ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
+    }
+  ];
+
+  constructor() {}
+
+  getAllRecipes() {
+    return [...this.recipes];
+  }
+
+  getRecipe(recipeId: string) {
+    return {
+      ...this.recipes.find(recipe => {
+        return recipe.id === recipeId;
+      })
+    };
+  }
+}
