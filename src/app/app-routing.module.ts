44e072dc0da9da24bbbcc6fcf42fc80bb39505be
Eli Alvarez <elialvarezromero11@gmail.com>
2019-11-28 10:26:23 -0600

    REFACTOR: Create module and service profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'bookings',
     loadChildren: './bookings/bookings.module#BookingsPageModule',
     canLoad: [AuthGuard]
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   }
 ];
 
