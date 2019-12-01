36bdad818d1bd034dae2f0f63538e6d0bc1764e5
Eli Alvarez <elialvarezromero11@gmail.com>
2019-11-30 23:59:26 -0600

    FEATURE: Send parameters form login

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 import { LoadingController } from '@ionic/angular';
-
 import { AuthService } from './auth.service';
 
 @Component({
@@ -10,7 +10,9 @@ import { AuthService } from './auth.service';
   styleUrls: ['./auth.page.scss']
 })
 export class AuthPage implements OnInit {
+
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService,
@@ -24,14 +26,33 @@ export class AuthPage implements OnInit {
     this.isLoading = true;
     this.authService.login();
     this.loadingCtrl
-      .create({ keyboardClose: true, message: 'Logging in...' })
+      .create({ keyboardClose: true, message: 'Iniciando...' })
       .then(loadingEl => {
         loadingEl.present();
         setTimeout(() => {
           this.isLoading = false;
           loadingEl.dismiss();
-          this.router.navigateByUrl('/places/tabs/discover');
+          this.router.navigateByUrl('/user');
         }, 1500);
       });
   }
+  /*
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
+  }
+  */
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email + ' ' + password);
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
+  }
+
 }
