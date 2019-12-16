68c8fa1db6c01bf0121ed9253233e298a79be77f
Eli Alvarez <elialvarezromero11@gmail.com>
2019-12-16 11:30:17 -0600

    VERSION: V 2.1

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 import { ProfileService } from '../services/profile.service';
 @Component({
   selector: 'app-profile',
@@ -6,30 +7,52 @@ import { ProfileService } from '../services/profile.service';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+
   constructor(
+    private loadingCtrl: LoadingController,
     private profileService: ProfileService
   ) {}
+
+  isLoading = true;
+  visible: any = false;
+  personId: any;
+  nombre: any;
+  numeroEmpleado: any;
+  unidad: any;
+  puesto: any;
+  cargo: any;
+  nivel: any;
+  rfc: any;
+  fechaIngresoFormat: any;
+  fotoBase64: any;
+
   ngOnInit() {
-    /*
-    const request                       = window.localStorage.getItem('user');
-    const response                      = JSON.parse(request);
-    this.profile.personId               = response.data.personId;
-    this.profile.nombre                 = response.data.nombre;
-    this.profile.numeroEmpleado         = response.data.numeroEmpleado;
-    this.profile.unidad                 = response.data.unidad;
-    this.profile.puesto                 = response.data.puesto;
-    this.profile.cargo                  = response.data.cargo;
-    this.profile.nivel                  = response.data.nivel;
-    this.profile.rfc                    = response.data.rfc;
-    this.profile.fechaIngresoFormat     = response.data.fechaIngresoFormat;
-    this.profile.fotoBase64             = response.data.fotoBase64;
-    */
     this.profile();
   }
 
-  profile() {
-    this.profileService.profile().subscribe((res) => {
-      console.log(JSON.stringify(res));
+  profile(): void {
+    this.isLoading = true;
+    this.loadingCtrl
+    .create({ keyboardClose: true, message: 'Cargando datos...' })
+    .then(loadingEl => {
+      loadingEl.present();
+      this.profileService.profile().subscribe((res: Response) => {
+        console.log(JSON.stringify(res));
+        this.personId           = res['data'].personId;
+        this.nombre             = res['data'].nombre;
+        this.numeroEmpleado     = res['data'].numeroEmpleado;
+        this.unidad             = res['data'].unidad;
+        this.puesto             = res['data'].puesto;
+        this.cargo              = res['data'].cargo;
+        this.nivel              = res['data'].nivel;
+        this.nivel              = res['data'].nivel;
+        this.rfc                = res['data'].rfc;
+        this.fechaIngresoFormat = res['data'].fechaIngresoFormat;
+        this.fotoBase64         = res['data'].fotoBase64;
+        this.visible            = true;
+        this.isLoading          = false;
+        loadingEl.dismiss();
+      });
     });
   }
 
