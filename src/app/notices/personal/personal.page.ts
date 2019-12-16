600c3fd8eac83567ec5fffc5e3829ba502f32369
Eli Alvarez <elialvarezromero11@gmail.com>
2019-12-16 15:06:54 -0600

    VERSION: V 3.6

--- src/app/notices/personal/personal.page.ts
+++ src/app/notices/personal/personal.page.ts
@@ -13,6 +13,7 @@ import { Constants as CONST } from '../../config/config.const';
   styleUrls: ['./personal.page.scss'],
 })
 export class PersonalPage implements OnInit {
+
   constructor(
     private loadingCtrl: LoadingController,
     private noticesService: NoticesService,
@@ -21,17 +22,20 @@ export class PersonalPage implements OnInit {
     private file: File,
     private fileOpener: FileOpener
   ) {}
+
   isLoading = false;
   isLogin = true;
   listPersonal: any[];
   base: any;
   idPerson = this.globalService.personId();
   visible: any = false;
+  nameFile: any;
+
   ngOnInit() {
     this.personal();
   }
+
   personal(): void {
-    this.isLoading = true;
     this.loadingCtrl
     .create({ keyboardClose: true, message: 'Cargando datos...' })
     .then(loadingEl => {
@@ -40,7 +44,6 @@ export class PersonalPage implements OnInit {
         this.listPersonal = res['data'];
         console.log(JSON.stringify(this.listPersonal));
         this.visible = true;
-        this.isLoading = false;
         loadingEl.dismiss();
       },
       (err) => {
@@ -59,32 +62,33 @@ export class PersonalPage implements OnInit {
       loadingEl.present();
       this.noticesService.download(id).subscribe( (res: {} ) => {
         this.base = res['data'];
+        this.nameFile = res['data'].nombreArchivo;
         if (extension === 'pdf') {
-          this.b64toBlob(this.base, CONST.APPLICATION_PDF, CONST.SIZE_BUFFER);
+          this.globalService.b64toBlob(this.base, this.nameFile, CONST.APPLICATION_PDF, CONST.SIZE_BUFFER);
         }
         if (extension === 'xls') {
-          this.b64toBlob(this.base, CONST.APPLICATION_XLS, CONST.SIZE_BUFFER);
+          this.globalService.b64toBlob(this.base, this.nameFile, CONST.APPLICATION_XLS, CONST.SIZE_BUFFER);
         }
         if (extension === 'xlsx') {
-          this.b64toBlob(this.base, CONST.APPLICATION_XLSX, CONST.SIZE_BUFFER);
+          this.globalService.b64toBlob(this.base, this.nameFile, CONST.APPLICATION_XLSX, CONST.SIZE_BUFFER);
         }
         if (extension === 'doc') {
-          this.b64toBlob(this.base, CONST.APPLICATION_DOC, CONST.SIZE_BUFFER);
+          this.globalService.b64toBlob(this.base, this.nameFile, CONST.APPLICATION_DOC, CONST.SIZE_BUFFER);
         }
         if (extension === 'docx') {
-          this.b64toBlob(this.base, CONST.APPLICATION_DOCX, CONST.SIZE_BUFFER);
+          this.globalService.b64toBlob(this.base, this.nameFile, CONST.APPLICATION_DOCX, CONST.SIZE_BUFFER);
         }
         if (extension === 'jpg') {
-          this.b64toBlob(this.base, CONST.APPLICATION_JPG, CONST.SIZE_BUFFER);
+          this.globalService.b64toBlob(this.base, this.nameFile, CONST.APPLICATION_JPG, CONST.SIZE_BUFFER);
         }
         if (extension === 'jpeg') {
-          this.b64toBlob(this.base, CONST.APPLICATION_JPEG, CONST.SIZE_BUFFER);
+          this.globalService.b64toBlob(this.base, this.nameFile, CONST.APPLICATION_JPEG, CONST.SIZE_BUFFER);
         }
         if (extension === 'png') {
-          this.b64toBlob(this.base, CONST.APPLICATION_PNG, CONST.SIZE_BUFFER);
+          this.globalService.b64toBlob(this.base, this.nameFile, CONST.APPLICATION_PNG, CONST.SIZE_BUFFER);
         }
         if (extension === 'txt') {
-          this.b64toBlob(this.base, CONST.APPLICATION_TXT, CONST.SIZE_BUFFER);
+          this.globalService.b64toBlob(this.base, this.nameFile, CONST.APPLICATION_TXT, CONST.SIZE_BUFFER);
         }
       },
       (err) => {
@@ -107,32 +111,6 @@ export class PersonalPage implements OnInit {
     console.log(ext);
     this.download(id, ext);
     slidingEl.close();
-  }
-
-  b64toBlob(b64Data: string, contentType: string, sliceSize: number): void {
-    const byteCharacters = atob(b64Data);
-    const byteArrays = [];
-    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-      const slice = byteCharacters.slice(offset, offset + sliceSize);
-      const byteNumbers = new Array(slice.length);
-      for (let i = 0; i < slice.length; i++) {
-        byteNumbers[i] = slice.charCodeAt(i);
-      }
-      const byteArray = new Uint8Array(byteNumbers);
-      byteArrays.push(byteArray);
-    }
-    const blob = new Blob(byteArrays, { type: contentType });
-    const fileName = 'file_excel';
-    const filePath = (this.platform.is('android')) ? this.file.externalRootDirectory : this.file.cacheDirectory;
-    this.file.writeFile(filePath, fileName, blob, { replace: true }).then((fileEntry) => {
-      this.fileOpener.open(fileEntry.toURL(), CONST.APPLICATION_XLSX)
-        .then(() => console.log('File is opened'))
-        .catch(err => console.error('Error openening file: ' + err));
-    })
-    .catch((err) => {
-      console.error('Error creating file: ' + err);
-      throw err;
-    });
   }
 
 }
