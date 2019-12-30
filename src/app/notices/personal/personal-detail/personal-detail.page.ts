3a611cb242e0e9a55268a4e6abe4d6ae47ca48e4
Eli Alvarez <elialvarezromero11@gmail.com>
2019-12-29 23:40:20 -0600

    VERSION: V 8.2

--- src/app/notices/personal/personal-detail/personal-detail.page.ts
+++ src/app/notices/personal/personal-detail/personal-detail.page.ts
@@ -4,7 +4,6 @@ import { NoticesService } from '../../../services/notices.service';
 import { GlobalService } from '../../../services/global.service';
 import { UtilsMessage } from '../../../utils/utils.message';
 import { UtilsNavigate } from '../../../utils/utils.navigate';
-import { UtilsHidden } from '../../../utils/utils.hidden';
 import { Constants as CONST } from '../../../config/config.const';
 import { ActivatedRoute } from '@angular/router';
 
@@ -18,12 +17,6 @@ export class PersonalDetailPage implements OnInit {
   id: any;
   nombreArchivo: any;
   mensaje: any;
-  isLoading = false;
-  isLogin = true;
-  listPersonal: any[];
-  base64: any;
-  idPerson = this.globalService.personId();
-  visible: any = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -32,7 +25,6 @@ export class PersonalDetailPage implements OnInit {
     private globalService: GlobalService,
     private utilsMessage: UtilsMessage,
     private utilsNavigate: UtilsNavigate,
-    private utilsHidden: UtilsHidden
   ) {}
 
   ngOnInit() {
@@ -42,9 +34,6 @@ export class PersonalDetailPage implements OnInit {
     this.id = id;
     this.nombreArchivo = nombreArchivo;
     this.mensaje = mensaje;
-    console.log(this.id);
-    console.log(this.nombreArchivo);
-    console.log(this.mensaje);
   }
 
   getFileExtension(nameFile: string) {
@@ -56,30 +45,28 @@ export class PersonalDetailPage implements OnInit {
     id = this.id;
     nameFile = this.nombreArchivo;
     const extension = this.getFileExtension(nameFile);
-    console.log(nameFile);
-    console.log(extension);
     this.loadingCtrl
     .create({ keyboardClose: true, message: this.utilsMessage.messageDownloading() })
     .then(loadingEl => {
       loadingEl.present();
       this.noticesService.download(id).subscribe( (res: Response ) => {
         const key = 'data';
-        this.base64 = res[key];
+        const base64 = res[key];
         switch (extension) {
           case 'pdf':
-            this.globalService.b64toBlobPdf(this.base64, nameFile, CONST.APPLICATION_PDF, CONST.SIZE_BUFFER);
+            this.globalService.b64toBlobPdf(base64, nameFile, CONST.APPLICATION_PDF, CONST.SIZE_BUFFER);
             break;
           case 'docx':
-            this.globalService.b64toBlobDocx(this.base64, nameFile, CONST.APPLICATION_DOCX, CONST.SIZE_BUFFER);
+            this.globalService.b64toBlobDocx(base64, nameFile, CONST.APPLICATION_DOCX, CONST.SIZE_BUFFER);
             break;
           case 'txt':
-            this.globalService.b64toBlobTxt(this.base64, nameFile, CONST.APPLICATION_TXT, CONST.SIZE_BUFFER);
+            this.globalService.b64toBlobTxt(base64, nameFile, CONST.APPLICATION_TXT, CONST.SIZE_BUFFER);
             break;
           case 'xlsx':
-            this.globalService.b64toBlobXlsx(this.base64, nameFile, CONST.APPLICATION_XLSX, CONST.SIZE_BUFFER);
+            this.globalService.b64toBlobXlsx(base64, nameFile, CONST.APPLICATION_XLSX, CONST.SIZE_BUFFER);
             break;
           case 'jpg':
-            this.globalService.b64toBlobJpg(this.base64, nameFile, CONST.APPLICATION_JPG, CONST.SIZE_BUFFER);
+            this.globalService.b64toBlobJpg(base64, nameFile, CONST.APPLICATION_JPG, CONST.SIZE_BUFFER);
             break;
           default:
             this.utilsMessage.messageNotAvaible();
--- src/app/notices/personal/personal.page.ts
+++ src/app/notices/personal/personal.page.ts
@@ -1,20 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
-import { IonItemSliding } from '@ionic/angular';
 import { NoticesService } from '../../services/notices.service';
 import { GlobalService } from '../../services/global.service';
 import { UtilsMessage } from '../../utils/utils.message';
 import { UtilsNavigate } from '../../utils/utils.navigate';
 import { UtilsHidden } from '../../utils/utils.hidden';
-import { Constants as CONST } from '../../config/config.const';
-
 @Component({
   selector: 'app-personal',
   templateUrl: './personal.page.html',
   styleUrls: ['./personal.page.scss'],
 })
 export class PersonalPage implements OnInit {
-
   constructor(
     private loadingCtrl: LoadingController,
     private noticesService: NoticesService,
@@ -23,18 +19,14 @@ export class PersonalPage implements OnInit {
     private utilsNavigate: UtilsNavigate,
     private utilsHidden: UtilsHidden
   ) {}
-
   isLoading = false;
   isLogin = true;
   listPersonal: any[];
-  base64: any;
   idPerson = this.globalService.personId();
   visible: any = false;
-
   ngOnInit() {
     this.personalInit();
   }
-
   personalInit(): void {
     this.loadingCtrl
     .create({ keyboardClose: true, message: this.utilsMessage.messageCharging() })
@@ -54,50 +46,4 @@ export class PersonalPage implements OnInit {
       });
     });
   }
-
-  getFileExtension(nameFile: string) {
-    const ext = /^.+\.([^.]+)$/.exec(nameFile);
-    return ext == null ? '' : ext[1];
-  }
-
-  download(id: string, nameFile: string): void {
-    const extension = this.getFileExtension(nameFile);
-    console.log(nameFile);
-    console.log(extension);
-    this.loadingCtrl
-    .create({ keyboardClose: true, message: this.utilsMessage.messageDownloading() })
-    .then(loadingEl => {
-      loadingEl.present();
-      this.noticesService.download(id).subscribe( (res: Response ) => {
-        const key = 'data';
-        this.base64 = res[key];
-        switch (extension) {
-          case 'pdf':
-            this.globalService.b64toBlobPdf(this.base64, nameFile, CONST.APPLICATION_PDF, CONST.SIZE_BUFFER);
-            break;
-          case 'docx':
-            this.globalService.b64toBlobDocx(this.base64, nameFile, CONST.APPLICATION_DOCX, CONST.SIZE_BUFFER);
-            break;
-          case 'txt':
-            this.globalService.b64toBlobTxt(this.base64, nameFile, CONST.APPLICATION_TXT, CONST.SIZE_BUFFER);
-            break;
-          case 'xlsx':
-            this.globalService.b64toBlobXlsx(this.base64, nameFile, CONST.APPLICATION_XLSX, CONST.SIZE_BUFFER);
-            break;
-          case 'jpg':
-            this.globalService.b64toBlobJpg(this.base64, nameFile, CONST.APPLICATION_JPG, CONST.SIZE_BUFFER);
-            break;
-          default:
-            this.utilsMessage.messageNotAvaible();
-        }
-        loadingEl.dismiss();
-      },
-      (err) => {
-        this.utilsMessage.messageApiError(err, 'PersonalPage', 'download()');
-        loadingEl.dismiss();
-        this.utilsNavigate.routerNavigateNotices();
-      });
-    });
-  }
-
 }
