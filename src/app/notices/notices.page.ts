6923a9c9fe6fd3b2b3c28f0463f6c840ee6a6621
Eli <elialvarezromero11@gmail.com>
2020-12-19 03:23:32 -0600

    UPDATE: Update ng8 to ng10

--- src/app/notices/notices.page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-notices',
-  templateUrl: './notices.page.html',
-  styleUrls: ['./notices.page.scss'],
-})
-export class NoticesPage {
-  constructor() {}
-}
